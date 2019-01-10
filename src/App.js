fdcbc8a25dc0eb0519b684146b8e58d74c930c0a
mattspoche <matthewspoche@comcast.net>
2019-01-09 19:58:02 -0600

    Github pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ClickItem from "./components/click-item";
+import Wrapper from "./components/wrapper";
+import Title from "./components/title";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        //clickables
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Clicky Game</Title>
+                <ClickItem></ClickItem>
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
